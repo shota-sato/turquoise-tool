1bfc15f4b4f492f5c2694cd7e73fe06b6d5579ba
Shoteen30h <bv13025@gmail.com>
2025-06-08 14:46:00 +0900

    Add files via upload

---
+++ spring/spring-data-elasticsearch/elasticdata/src/main/java/com/example/elasticdata/ElasticdataApplication.java
@@ -0,0 +1,13 @@
+package com.example.elasticdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElasticdataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElasticdataApplication.class, args);
+	}
+
+}
