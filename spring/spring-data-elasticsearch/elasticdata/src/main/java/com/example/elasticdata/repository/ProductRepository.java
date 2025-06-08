64cc7db0d5e30cf7b6db93137641d8c688c666bd
Shoteen30h <bv13025@gmail.com>
2025-06-08 14:57:34 +0900

    Delete spring/spring-data-elasticsearch/elasticdata directory

--- spring/spring-data-elasticsearch/elasticdata/src/main/java/com/example/elasticdata/repository/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.elasticdata.repository;
-
-import com.example.elasticdata.model.Product;
-import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
-
-public interface ProductRepository extends ElasticsearchRepository<Product, String> {
-}
